d00d1953bfcf21db2435329c1d4e91bc0ffbfe3a
harshalgupta090 <harshalgupta090@gmail.com>
2023-04-17 21:45:55 +0530

    Added attribute directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,15 +2,34 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+
+        .online{
+         color: white   
+        }
+        .offline{
+            color: cyan
+
+        }
+    `]
 })
 export class ServerComponent{
 
     serverID: number = 10;
-    private status: string = 'offline';
+    public status: string = 'offline';
+   
+    connstructor(){
+        this.status = Math.random() > 0.5 ? 'online' : 'offline';
+        console.log(this.status);
+    }
+
     getServerStatus(): String{
         return this.status
     }
 
+    getColor(): string{
+        return this.status === 'online' ?'green' : 'red';
+    }
 
 }
